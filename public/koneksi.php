475d6f96320c0e038be32e7bfb9bc00b4d41915e
Steven <56438848+StvnRD@users.noreply.github.com>
2021-07-24 08:51:18 +0700

    Add files via upload

---
+++ public/koneksi.php
@@ -0,0 +1,7 @@
+<?php
+    $host ="localhost"; //host server
+    $user ="id17281704_antrian"; //user login phpMyAdmin
+    $pass ="Excellord0001~"; //pass login phpMyAdmin
+    $db ="id17281704_lab_ci4"; //nama database
+    $conn = mysqli_connect($host, $user, $pass, $db) or die ("Koneksi gagal");
+?>
